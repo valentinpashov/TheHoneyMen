5bbc7684426d8387941a5d2761fd81b7261e45df
Valentin Pashov <valentinpashov04@gmail.com>
2026-01-24 18:49:30 +0200

    Add Navbar component

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">TheHoneyMen 🐝</div>
+      <ul className="navbar-links">
+        <li><a href="#home">Начало</a></li>
+        <li><a href="#products">Продукти</a></li>
+        <li><a href="#about">За нас</a></li>
+      </ul>
+      <div className="navbar-cart">
+        <span>🛒</span>
+        <span className="cart-count">0</span>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
