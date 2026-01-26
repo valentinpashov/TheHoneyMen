0895464e265f47ced04ac993e7a6d16220603101
Valentin Pashov <valentinpashov04@gmail.com>
2026-01-26 17:29:01 +0200

    Add "add product function"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
@@ -5,11 +6,37 @@ import ProductList from './components/ProductList';
 import Footer from './components/Footer';
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+  
+  const [notification, setNotification] = useState(null);
+
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  // Add function
+  const handleAddToCart = (product) => {
+    //Add product to cart
+    setCartItems(prevItems => [...prevItems, product]);
+    
+    // Show notification
+    setNotification(`Добавихте "${product.name}" в количката! 🍯`);
+    
+    setTimeout(() => {
+      setNotification(null);
+    }, 3000);
+  };
+
+  const toggleCart = () => {
+    setIsCartOpen(!isCartOpen);
+  };
+
   return (
     <div className="App">
-      <Navbar />
+      
+      
       <Hero />
-      <ProductList />
+      
+      <ProductList addToCart={handleAddToCart} />
+      
       <Footer />
     </div>
   );
