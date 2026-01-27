ebfe2a3a96bba978a2a9e928ef3b2c956b4eabed
Valentin Pashov <valentinpashov04@gmail.com>
2026-01-27 15:43:58 +0200

    Edit Hero moment

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -3,13 +3,23 @@ import './Hero.css';
 
 function Hero() {
   return (
-    <header className="hero-section" id="home">
+    <section className="hero-section" id="home">
+      
+      <div 
+        className="hero-bg-image"
+        style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/images/winter.png)` }}
+      ></div>
+
+      <div className="hero-overlay"></div>
+      
       <div className="hero-content">
-        <h1>Истински пчелен мед</h1>
-        <h2>Директно от кошера до твоята трапеза</h2>
-        <button className="cta-button">Поръчай сега</button>
+        <h1>Златна топлина за ледени дни</h1>
+        <h2>Зареди се с натурален имунитет директно от кошера</h2>
+        <a href="#products">
+            <button className="cta-button">Поръчай сега</button>
+        </a>
       </div>
-    </header>
+    </section>
   );
 }
 
