3faa3bea9456ac6c00998c9bef5bab84c2cd417f
Valentin Pashov <valentinpashov04@gmail.com>
2026-01-29 15:00:29 +0200

    Add about section

---
+++ frontend/src/components/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <section className="about-section" id="about">
+      <div className="about-container">
+        
+        {/* Introduction */}
+        <div className="about-header">
+          <span className="subtitle">Нашата история</span>
+          <h2>Повече от мед. <br /> Това е наследство.</h2>
+          <div className="gold-divider"></div>
+        </div>
+
+
+
+      </div>
+    </section>
+  );
+}
+
+export default About;
