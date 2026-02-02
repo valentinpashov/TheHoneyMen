87297f1bd3df497e4a6551dddc61544ddf312d73
Valentin Pashov <valentinpashov04@gmail.com>
2026-02-02 14:05:56 +0200

    Add 404 Error page

---
+++ frontend/src/components/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css';
+
+function NotFound() {
+  return (
+    <div className="not-found-container">
+      <div className="bee-emoji">🐝</div>
+      <h1>404</h1>
+
+    </div>
+  );
+}
+
+export default NotFound;
