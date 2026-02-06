13318f2947c2f062cc58f20a14313f262146a79a
Valentin Pashov <valentinpashov04@gmail.com>
2026-02-06 17:14:46 +0200

    Add project's case

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'; 
+import { BrowserRouter } from 'react-router-dom';
+import { LanguageProvider } from './LanguageContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <LanguageProvider> 
+        <App />
+      </LanguageProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
